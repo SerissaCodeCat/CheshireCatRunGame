d9db7c1bcf66a1b583a7e91cbd115b99094a3215
Serissa Smith <blacksnowreturns@gmail.com>
2024-09-17 15:20:44 -0400

    added basic patrol between two walls to enemies as a self contained script. TODO: - do not bounce off player - accelerate to full speed when player spotted - stop before hitting wall to look less like a robot - Maybe add random pauses in patrol path?

--- GenericScripts/EnemyPatrol.cs
+++ GenericScripts/EnemyPatrol.cs
@@ -6,11 +6,13 @@ public partial class EnemyPatrol : CharacterBody2D
 {
 
 	public const float Speed = 350.0f;
+	public const float acceleration = 15.0f;
 	private bool wallToRight = false;
 	private Godot.Vector2 finalVelocity;
 	private bool direction = true;
 	private Godot.Vector2 Stop = new Godot.Vector2(0, 0);
-	private CollisionShape2D Collision;
+	private CollisionShape2D CollisionBody;
+	private Area2D AreaDetection;
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	private AnimatedSprite2D sprite_2d;
 
@@ -18,20 +20,35 @@ public partial class EnemyPatrol : CharacterBody2D
 	public override void _Ready()
 	{
 		sprite_2d = GetNode<AnimatedSprite2D>($"Sprite2D");
-		Collision = GetNode<CollisionShape2D>($"CollisionShapeStanding");
-
+		CollisionBody = GetNode<CollisionShape2D>($"CollisionShapeStanding");
+		AreaDetection = GetNode<Area2D>($"DetectionArea");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		finalVelocity = Velocity;
+		
 		Move(ref finalVelocity, delta);
+		
+		Velocity = finalVelocity;
+		GD.Print("final velocity: " + Velocity);
 		MoveAndSlide();
+		if (IsOnWall())
+		{
+			direction = !direction;
+		}
 	}
 
 	private void Move(ref Godot.Vector2 incomingVelocity, double incomingDelta)
 	{
+		
 		incomingVelocity.Y += gravity * (float)incomingDelta;
+		if (IsOnFloor())
+		{
+			incomingVelocity.X = Mathf.MoveToward(incomingVelocity.X, (direction? 1.0f: -1.0f) * (Speed / 2), acceleration);
+			sprite_2d.FlipH = !direction;		
+		}
+
 	}
 }
